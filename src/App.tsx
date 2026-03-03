18e04dc6c4384911b9451800b98b6553ad216929
Vini Salazar <17276653+vinisalazar@users.noreply.github.com>
2026-03-03 22:28:40 +1100

    Debugging GH Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route element={<Layout />}>
             <Route path="/" element={<Index />} />
